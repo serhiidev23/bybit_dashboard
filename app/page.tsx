1218e65aa9b3a8416524a237043538d9a41c5508
ithunter0428 <viperhunter901@gmail.com>
2023-11-23 11:44:10 +0300

    complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,185 +1,29 @@
 'use client'
-import { useEffect } from 'react';
-import useWebSocket from 'react-use-websocket';
-import { WebsocketClient, WSClientConfigurableOptions, RestClientV5 } from 'bybit-api';
-
-// import url from 'url';
-// import WebSocket from 'ws';
-// import crypto from 'crypto';
-// var endpoint = "wss://stream.bybit.com/v5/private"
-
-const API_KEY = '5kCdfNpPLhXcBmx5tr'
-const PRIVATE_KEY = 'ddE307lw8zbsPaaLSpVApuJifnRs3LGBjjo4'
-
-const wsConfig: WSClientConfigurableOptions = {
-  key: API_KEY,
-  secret: PRIVATE_KEY,
-  market: 'v5',
-};
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import SubAccount from '@/components/SubAccount';
 
 export default function Home() {
-  // const { sendJsonMessage } = useWebSocket(endpoint, {
-  //   onOpen: () => {
-  //     console.log('"open" event!');
-  //     console.log('WebSocket Client Connected');
-  //     const expires = new Date().getTime() + 100000;
-  //     const signature = crypto.createHmac("sha256", PRIVATE_KEY).update("GET/realtime" + expires).digest("hex");
-  //     const payload={
-  //       op: "auth",
-  //       args: [API_KEY, expires.toFixed(0), signature],
-  //     }
-  //     sendJsonMessage(payload);
-  //     sendJsonMessage({"req_id": "100001", "op": "ping"});
-  //     sendJsonMessage({op: "subscribe", args: ['position']});
-      
-  //   },
-  //   onMessage: (data: any) => {
-  //     console.log(data);
-  //   }
-  // });
+  const [accounts, setAccounts] = useState([]);
 
   useEffect(() => {
-    const ws = new WebsocketClient(wsConfig);
-
-    ws.subscribeV5('position', 'linear');
-
-    ws.on('update', data => {
-      console.log('update', data);
-    });
-    
-    // Optional: Listen to websocket connection open event (automatic after subscribing to one or more topics)
-    ws.on('open', ({ wsKey, event }) => {
-      console.log('connection open for websocket with ID: ' + wsKey);
-      // ws.tryWsSend(wsKey, 'ping')
-    });
-    
-    // Optional: Listen to responses to websocket queries (e.g. the response after subscribing to a topic)
-    ws.on('response', response => {
-      console.log('response', response);
-    });
-    
-    // Optional: Listen to connection close event. Unexpected connection closes are automatically reconnected.
-    ws.on('close', () => {
-      console.log('connection closed');
-    });
-    
-    // Optional: Listen to raw error events.
-    // Note: responses to invalid topics are currently only sent in the "response" event.
-    ws.on('error', err => {
-      console.error('ERR', err);
-    });
+    axios.get('https://api.retool.com/v1/workflows/f21a6ecb-29cb-4356-b09a-018d5eaef774/startTrigger?workflowApiKey=retool_wk_53c0b0fc844e4f168e0fb2abf2183197')
+    .then((data: any) => {
+      setAccounts(data.data);
+    })
 
     return 
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="flex md:p-24 min-h-screen sm:p-5">
+      {accounts.map((account: any, index) => 
+        <SubAccount 
+          key={index} 
+          API_KEY={account.API_KEY} 
+          PRIVATE_KEY={account.PRIVATE_KEY} 
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
-      
+      )}
     </main>
   )
 }
---
+++ components/SubAccount.tsx
@@ -0,0 +1,156 @@
+'use client'
+import { useEffect, useState } from 'react';
+import { WebsocketClient, WSClientConfigurableOptions, RestClientV5 } from 'bybit-api';
+
+export default function SubAccount({ API_KEY, PRIVATE_KEY }: any) {
+	const [info, setInfo] = useState({
+		symbol: '',
+		leverage: '',
+		size: '',
+		entryPrice: '',
+		markPrice: '',
+		liqPrice: '',
+		unrealisedPnl: ''
+	})
+	const [error, setError] = useState('')
+
+	useEffect(() => {
+		const getInfo = () => {
+			const restClient = new RestClientV5({
+				key: API_KEY,
+				secret: PRIVATE_KEY,
+				recv_window: 2
+			});
+
+			restClient.getPositionInfo({ category: 'linear', symbol: 'ATOMUSDT' })
+			.then(result => {
+				console.log("getPositionInfo result: ", result);
+				if (result.result.list) {
+					const payload: any = result.result.list[0];
+					setInfo({
+						symbol: payload.symbol,
+						leverage: payload.leverage,
+						size: payload.size,
+						entryPrice: payload.entryPrice,
+						markPrice: payload.markPrice,
+						liqPrice: payload.liqPrice,
+						unrealisedPnl: payload.unrealisedPnl
+					})
+					setError('')
+				}
+				else {
+					setError(result.retMsg)
+				}
+			})
+			.catch(err => {
+				console.error("getPositionInfo error: ", err);
+			});
+		}
+
+		getInfo();
+
+		const wsConfig: WSClientConfigurableOptions = {
+			key: API_KEY,
+			secret: PRIVATE_KEY,
+			market: 'v5'
+		};
+
+		const ws = new WebsocketClient(wsConfig);
+
+		ws.subscribeV5(['position'], 'linear');
+
+		ws.on('update', data => {
+			console.log('update', data);
+			const payload = data.data[0];
+			setInfo({
+				symbol: payload.symbol,
+				leverage: payload.leverage,
+				size: payload.size,
+				entryPrice: payload.entryPrice,
+				markPrice: payload.markPrice,
+				liqPrice: payload.liqPrice,
+				unrealisedPnl: payload.unrealisedPnl
+			})
+		});
+		
+		// Optional: Listen to websocket connection open event (automatic after subscribing to one or more topics)
+		ws.on('open', ({ wsKey, event }) => {
+			console.log('connection open for websocket with ID: ' + wsKey);
+			// ws.tryWsSend(wsKey, 'ping')
+		});
+		
+		// Optional: Listen to responses to websocket queries (e.g. the response after subscribing to a topic)
+		ws.on('response', response => {
+			console.log('response', response);
+		});
+		
+		// Optional: Listen to connection close event. Unexpected connection closes are automatically reconnected.
+		ws.on('close', () => {
+			console.log('connection closed');
+		});
+		
+		// Optional: Listen to raw error events.
+		// Note: responses to invalid topics are currently only sent in the "response" event.
+		ws.on('error', err => {
+			console.error('ERR', err);
+		});
+
+		return 
+	}, [])
+
+	return error?<>{error}</>: (<div className='flex-col items-center justify-between w-full py-6 h-64'>
+		<div className="md:flex justify-between w-full py-5">
+			<div>
+				<div className="relative flex place-items-center">
+					<span className='text-2xl mx-2'>{info.symbol}</span> 
+					<span className='text-green-300 bg-emerald-900 rounded-sm opacity-75'>Long</span>
+				</div>
+				<div className="relative flex place-items-center mx-2">
+					Cross {Number(info.leverage).toFixed(2)}x
+				</div>
+			</div>
+			<div>
+				<div className="text-lg relative flex place-items-center opacity-70 mx-2">
+					Unrealized P&L
+				</div>
+				<div className="text-2xl relative text-right mx-2 text-red-400	text-right">
+					{Number(info.unrealisedPnl).toFixed(2)}
+				</div>
+			</div>
+		</div>
+		<div className="md:flex justify-between w-full">
+			<div>
+				<div className="text-lg relative flex place-items-center opacity-70 mx-2">
+					Position Size
+				</div>
+				<div className="text-2xl relative flex place-items-center mx-2 ">
+					{Number(info.size).toFixed(2)}
+				</div>
+			</div>
+			<div>
+				<div className="text-lg relative flex place-items-center opacity-70 mx-2">
+					Entry Price
+				</div>
+				<div className="text-2xl relative flex place-items-center mx-2 ">
+					{Number(info.entryPrice).toFixed(2)}
+				</div>
+			</div>
+			<div>
+				<div className="text-lg relative flex place-items-center opacity-70 mx-2">
+					Mark Price
+				</div>
+				<div className="text-2xl relative flex place-items-center mx-2 ">
+					{Number(info.markPrice).toFixed(2)}
+				</div>
+			</div>
+			<div>
+				<div className="text-lg relative flex place-items-center opacity-70 mx-2">
+					Estimated Liq. Price
+				</div>
+				<div className="text-2xl relative text-right mx-2 text-amber-400">
+					{Number(info.liqPrice).toFixed(2)}
+				</div>
+			</div>
+		</div>
+	</div>)
+}
